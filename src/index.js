575d719a34e815b7892d1a99e3d7d0273feef48a
pandasoft0 <pandasoft0@gmail.com>
2020-03-22 22:53:22 -0300

    Primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {FormattedNumber,IntlProvider} from 'react-intl';
+
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class ValorDescrito extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      desc: props.desc,
+      valor: props.valor
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <span>{this.state.desc}</span> 
+        <span>
+          <FormattedNumber value={this.state.valor} style="currency" currency="BRL"/> 
+        </span>
+      </div>
+    )
+  }
+}
+
+
+
+
+// ========================================
+class Planinha extends React.Component{
+
+  render(){
+    return (
+      <IntlProvider locale="pt">
+        <ValorDescrito desc="Almoço" valor="123"/>
+      </IntlProvider>
+      );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <IntlProvider locale="pt">
+    <Planinha/>
+  </IntlProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
